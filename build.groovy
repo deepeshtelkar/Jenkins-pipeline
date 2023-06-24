a1df2b466f5f2955e12dbb6fde3e51545c02ffc2
deepeshtelkar <31217469+deepeshtelkar@users.noreply.github.com>
2023-06-24 00:46:51 -0400

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -22,5 +22,17 @@ pipeline {
                 sh 'java -jar target/*.jar'
             }
         }
+
+        stage('Deploy to AWS Server') {
+            steps {
+                script {
+                    // Copy JAR file to AWS server
+                    //sh 'scp -i /path/to/key.pem target/*.jar ec2-user@your-aws-server-ip:/path/to/deployed.jar'
+                    
+                    // SSH into AWS server and deploy the JAR file
+                    //sh 'ssh -i /path/to/key.pem ec2-user@your-aws-server-ip "java -jar /path/to/deployed.jar"'
+                }
+            }
+        }
     }
 }
